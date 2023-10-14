ec11291335dda8f2913c243310d6ea9f1fb34db7
TrevorMickelson <social@trevmick.com>
2023-10-14 17:39:17 -0500

    module3

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,10 @@
 const packageInfo = require('../../package.json');
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 const travel = (req, res) => {
-    res.render('travel', {title: packageInfo.description + ' - Travel'});
+    pageTitle = packageInfo.description + ' - Travel';
+    res.render('travel', {title: pageTitle, trips});
 };
 
 module.exports = {
